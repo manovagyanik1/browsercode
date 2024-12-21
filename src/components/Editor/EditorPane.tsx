db68f5ba121bb6db9d3cd29ee77bf7d3948b788b
manovagyanik1 <52470178+manovagyanik1@users.noreply.github.com>
2024-12-22 02:57:55 +0530

    Update src/components/Editor/EditorPane.tsx [BoltSync]

--- src/components/Editor/EditorPane.tsx
+++ src/components/Editor/EditorPane.tsx
@@ -1,79 +1,50 @@
-import Editor, { useMonaco } from '@monaco-editor/react';
-import { useEffect, useCallback } from 'react';
+import Editor from '@monaco-editor/react';
 import { useFileSystem } from '../../context/FileSystemContext';
-import { EDITOR_OPTIONS } from '../../constants/editorDefaults';
-import { getFileLanguage } from '../../utils/fileUtils';
-import { getFileContent } from '../../utils/fileSystemUtils';
 import { EmptyEditor } from './EmptyEditor';
 import { EditorHeader } from './EditorHeader';
-import { setupMonacoTypescript } from '../../utils/monacoUtils';
-import { getWebContainerInstance } from '../../services/webcontainer';
-import debounce from 'lodash/debounce';
+import { isFileEditable, getFileLanguage } from '../../utils/fileUtils';
+import { getFileContent } from '../../utils/fileSystemUtils';
+import { EDITOR_OPTIONS } from '../../constants/editorDefaults';
+import { darkTheme } from '../../constants/editorTheme';
+import { useEffect } from 'react';
+import type { Monaco } from '@monaco-editor/react';
 
 export function EditorPane() {
-  const monaco = useMonaco();
-  const { currentFile, files, updateFileContent, setCurrentFile } = useFileSystem();
+  const { currentFile, files, updateFileContent } = useFileSystem();
+  
+  const handleEditorMount = (editor: any, monaco: Monaco) => {
+    monaco.editor.defineTheme('browsercode-dark', darkTheme);
+    monaco.editor.setTheme('browsercode-dark');
+  };
 
-  // Debounced save function
-  const debouncedSave = useCallback(
-    debounce(async (path: string, content: string) => {
-      try {
-        const webcontainer = await getWebContainerInstance();
-        await webcontainer.fs.writeFile(path, content);
-      } catch (error) {
-        console.error('Failed to save file:', error);
-      }
-    }, 1000),
-    []
-  );
+  // Don't show editor for directories or non-editable files
+  if (!currentFile || !isFileEditable(currentFile)) {
+    return <EmptyEditor />;
+  }
 
-  useEffect(() => {
-    if (monaco) {
-      setupMonacoTypescript(monaco, files);
-    }
-  }, [monaco, files]);
-
-  const handleEditorChange = async (value: string | undefined) => {
+  const handleEditorChange = (value: string | undefined) => {
     if (currentFile && value !== undefined) {
       updateFileContent(currentFile, value);
-      debouncedSave(currentFile, value);
     }
   };
 
   return (
     <div className="w-full h-full flex flex-col bg-[#1e1e1e] min-w-0">
       <EditorHeader />
-      <div className="flex-1 min-h-0 relative">
-        {!currentFile ? (
-          <EmptyEditor />
-        ) : (
-          <Editor
-            height="100%"
-            defaultLanguage={getFileLanguage(currentFile)}
-            value={getFileContent(currentFile, files)}
-            theme="vs-dark"
-            options={{
-              ...EDITOR_OPTIONS,
-              quickSuggestions: {
-                other: true,
-                comments: true,
-                strings: true
-              },
-              suggestOnTriggerCharacters: true,
-              acceptSuggestionOnCommitCharacter: true,
-              wordBasedSuggestions: "allDocuments",
-              parameterHints: {
-                enabled: true
-              },
-              mouseWheelZoom: true,
-              smoothScrolling: true,
-              links: true,
-              minimap: { enabled: false }
-            }}
-            onChange={handleEditorChange}
-            path={currentFile}
-          />
-        )}
+      <div className="flex-1 min-h-0">
+        <Editor
+          height="100%"
+          defaultLanguage={getFileLanguage(currentFile)}
+          value={getFileContent(currentFile, files)}
+          theme="browsercode-dark"
+          options={{
+            ...EDITOR_OPTIONS,
+            readOnly: !isFileEditable(currentFile)
+          }}
+          onChange={handleEditorChange}
+          onMount={handleEditorMount}
+          path={currentFile}
+        />
       </div>
     </div>
   );
