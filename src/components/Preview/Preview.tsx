988cfa06576c80ef11ae9583adb58452c194bbf9
manovagyanik1 <52470178+manovagyanik1@users.noreply.github.com>
2024-12-21 23:37:24 +0530

    Update src/components/Preview/Preview.tsx [BoltSync]

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,22 +1,11 @@
-import { useFileSystem } from '../../context/FileSystemContext';
-
 export function Preview() {
-  const { currentFile } = useFileSystem();
-
-  if (!currentFile) {
-    return (
-      <div className="h-full flex items-center justify-center text-gray-500">
-        <p>Select a file to preview</p>
-      </div>
-    );
-  }
-
   return (
     <div className="h-full bg-white">
       <iframe
-        src={`/preview/${currentFile}`}
+        src="about:blank"
         className="w-full h-full border-none"
         title="Preview"
+        sandbox="allow-scripts allow-same-origin"
       />
     </div>
   );
